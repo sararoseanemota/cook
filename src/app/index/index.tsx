ca33916a6f59c70f34759a126371ef30752f6f02
sararoseanesm@gmail.com <sararoseanemota>
2024-03-07 15:01:52 -0300

    commit

---
+++ src/app/index/index.tsx
@@ -0,0 +1,73 @@
+import { View, Text, ScrollView, Alert } from "react-native";
+import { useState, useEffect } from "react";
+import { router } from "expo-router"
+import { Selected } from "@/components/Selected";
+import { Ingredient } from "@/components/Ingredient";
+import { styles } from "./styles";
+
+import { services } from "@/services";
+
+export default function Index() {
+  const [selected, setSelected] = useState<string[]>([]);
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([])
+
+  function handleToggleSelected(value: string) {
+    if (selected.includes(value)) {
+      return setSelected((state) => state.filter((item) => item !== value));
+    }
+
+    setSelected((state) => [...state, value]);
+  }
+
+  function handleClearSelected() {
+    Alert.alert("Limpar", "Deseja limpar tudo", [
+      { text: "Não", style: "cancel" },
+      { text: "Sim", onPress: () => setSelected([]) },
+    ]);
+  }
+
+  function handleSearch() {
+    router.navigate('/recipes/' + selected)
+    
+  }
+
+  useEffect(() => {
+    services.ingredients.findAll().then(setIngredients)
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>
+        Escolha {"\n"}
+        <Text style={styles.subtitle}>os produtos</Text>
+      </Text>
+
+      <Text style={styles.message}>
+        Descubra receitas baseadas nos produtos que você escolheu.
+      </Text>
+
+      <ScrollView
+        contentContainerStyle={styles.ingredients}
+        showsVerticalScrollIndicator={false}
+      >
+        {ingredients.map((item) => (
+          <Ingredient
+            key={item.id}
+            name={item.name}
+            image={`${services.storage.imagePath}/${item.image}`}
+            selected={selected.includes(item.id)}
+            onPress={() => handleToggleSelected(item.id)}
+          />
+        ))}
+      </ScrollView>
+
+      {selected.length > 0 && (
+        <Selected
+          quantity={selected.length}
+          onClear={handleClearSelected}
+          onSearch={handleSearch}
+        />
+      )}
+    </View>
+  );
+}
---
+++ src/app/recipes/[ingredientsIds].tsx
@@ -0,0 +1,49 @@
+import { View, Text, FlatList } from "react-native";
+import { router, useLocalSearchParams } from "expo-router";
+import { styles } from "./styles";
+import { MaterialIcons } from "@expo/vector-icons";
+import { Recipe } from "@/components/Recipe";
+import { useEffect, useState } from "react";
+import { Ingredients } from "@/components/Ingredients";
+import { services } from "@/services";
+
+export default function Recipes() {
+  const [ingredients, setIngredients] = useState<IngredientResponse[]>([])
+  const [recipes, setRecipes] = useState<RecipeResponse[]>([])
+  const params = useLocalSearchParams<{ingredientsIds: string}>()
+  const ingredientsIds = params.ingredientsIds.split(",")
+
+  useEffect(() => {
+    services.ingredients.findByIds(ingredientsIds).then(setIngredients)
+  }, [])
+
+  useEffect(() => {
+    services.recipes.findByIngredientsIds(ingredientsIds).then(setRecipes)
+  }, [])
+
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <MaterialIcons
+          name="arrow-back"
+          size={32}
+          onPress={() => router.back()}
+        />
+        <Text style={styles.title}>Ingredientes</Text>
+      </View>
+
+      <Ingredients ingredients={ingredients} />
+
+      <FlatList
+          data={recipes}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <Recipe recipe={item} onPress={() => router.navigate("/recipe/" + item.id)}/>}
+          style={styles.recipes}
+          contentContainerStyle={styles.recipesContent}
+          showsVerticalScrollIndicator={false}
+          columnWrapperStyle={{gap: 16}}
+          numColumns={2}
+      />
+    </View>
+  );
+}
